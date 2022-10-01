dace1c4528e4a92ff8eba6c59dc08dfae29f82d8
Parham2002 <parhamm2002@gmail.com>
2022-10-01 19:17:08 +0100

    Added functionality for updating dash word

--- src/main/java/org/example/HangmanUtils.java
+++ src/main/java/org/example/HangmanUtils.java
@@ -5,13 +5,13 @@ import java.util.Random;
 
 public class HangmanUtils {
     static ArrayList<String> listOfWords = new ArrayList<>();
+    private static int lives = 3;
 
     public static String pickAWord(ArrayList<String> word) {
         Random random = new Random();
         String targetWord = word.get(random.nextInt(word.size()));
         return targetWord;
     }
-
 
     static {
         listOfWords.add("pizza");
@@ -22,18 +22,36 @@ public class HangmanUtils {
         listOfWords.add("knowledge");
 
     }
-    public static String wordToDashes(String targetWord) {
+    public static String wordToDashes(String[] targetWord) {
         String dashWord = "";
-        for (int i = 0; i < targetWord.length(); i++) {
+        for (int i = 0; i < targetWord.length; i++) {
             dashWord += "_";
         }
         return dashWord;
     }
-    public static boolean checkGuess(String guessLetter, String targetWord) {
-        if (targetWord.contains(guessLetter)) {
-            return true;
+    public static boolean checkGuess(String guessLetter, String[] targetWord) {
+        for ( String letter : targetWord) {
+            if (letter.equals(guessLetter)) {
+                return true;
+            }
         }
+        lives--;
         return false;
+
     }
 
+    public static String updateDashWord(String guessLetter, String[] targetWord){
+        String updatedDashWord = "";
+        for (int i = 0; i < targetWord.length; i++) {
+            if (targetWord[i].equals(guessLetter)) {
+                updatedDashWord += guessLetter;
+            }
+            updatedDashWord += "_";
+        }
+        return updatedDashWord;
+    }
+
+    public static int getLives() {
+        return lives;
+    }
 }
